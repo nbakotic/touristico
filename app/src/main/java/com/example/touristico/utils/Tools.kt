cf2c792dbb30b88818b3c785ebead6e8f7facd3c
nbakotic <nbakotic@riteh.hr>
2022-12-03 18:16:22 +0100

    Added another table in DBHelper for storing images along with add and get functions. Added bitmapTOByteArray and getFileName functions to Tools. Function uploadPictureToFirebase from AdminAddBeachFragment changed so the image is added to internal database. BeachesAdapter edited so image view shows the image stored in the database.

--- app/src/main/java/com/example/touristico/utils/Tools.kt
+++ app/src/main/java/com/example/touristico/utils/Tools.kt
@@ -1,6 +1,12 @@
 package com.example.touristico.utils
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.OpenableColumns
+import java.io.ByteArrayOutputStream
 
 class Tools {
     companion object {
@@ -15,6 +21,35 @@ class Tools {
             intent.type = "image/*"
             intent.action = Intent.ACTION_GET_CONTENT
             return intent
+        }
+
+        fun bitmapTosByteArray(bitmap : Bitmap) : ByteArray {
+            val outputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, outputStream)
+            return outputStream.toByteArray()
+        }
+
+        @SuppressLint("Range")
+        fun getFileName (uri : Uri, context : Context) : String?{
+            var result : String? = null
+            if (uri.scheme.equals("content")) {
+                val cursor = context.contentResolver.query(uri, null, null, null, null)
+                try {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    }
+                } finally {
+                    cursor?.close()
+                }
+            }
+            if (result == null) {
+                result = uri.path
+                val cut : Int? = result?.lastIndexOf('/')
+                if (cut != null) {
+                    result = result?.substring(cut + 1)
+                }
+            }
+            return result
         }
     }
 }
